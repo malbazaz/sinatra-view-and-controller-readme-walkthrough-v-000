e07529d2c947238546e611bbd3598400f1eeef6b
Mo Al <malbazaz@mba2018.hbs.edu>
2018-07-26 21:13:35 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,12 +3,15 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/reverse' do
-    original_string = params["string"]
- @reversed_string = original_string.reverse
- "#{@reversed_string}"
-    erb :reversed
+    erb :reverse
   end
 
+  post '/reverse' do
+  original_string = params["string"]
+  @reversed_string = original_string.reverse
+
+  erb :reversed
+end
 
   get '/friends' do
     @friends = ['Emily Wilding Davison', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth']
